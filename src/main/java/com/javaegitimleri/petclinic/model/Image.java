d1689814c7814e7e0edd3337188c50610a8f5fd9
salbayrak <salbayrak@ebay.com>
2013-05-26 16:02:24 +0300

    usage of ElementCollection and MapKeyColumn annotations

--- src/main/java/com/javaegitimleri/petclinic/model/Image.java
+++ src/main/java/com/javaegitimleri/petclinic/model/Image.java
@@ -1,17 +1,21 @@
 package com.javaegitimleri.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.io.Serializable;
 
+@Embeddable
 public class Image implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+
 	private String filename;
 	
 	private int width;
 	
 	private int height;
-	
+
+    @Column(name = "FILENAME")
 	public String getFilename() {
 		return filename;
 	}
